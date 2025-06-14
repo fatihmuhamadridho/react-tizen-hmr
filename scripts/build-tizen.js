f1912c46819cc4e4f857dac6c22e4c10698c8498
fatihmuhamadridho <fatihmuhamadridho1@gmail.com>
2025-06-14 23:05:53 +0700

    feat: 🎸 add dotenv, env and new scripts

---
+++ scripts/build-tizen.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const { execSync } = require("child_process");
+require("dotenv").config(); // Load .env
+
+const profile = process.env.PROFILE;
+
+if (!profile) {
+  console.error("PROFILE not defined in .env");
+  process.exit(1);
+}
+
+execSync(`tizen package -t wgt -s ${profile} -o ./build -- ./build`, {
+  stdio: "inherit",
+});
---
+++ scripts/install-tizen.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const { execSync } = require("child_process");
+require("dotenv").config();
+
+const device = process.env.DEVICE;
+
+if (!device) {
+  console.error("DEVICE not defined in .env");
+  process.exit(1);
+}
+
+execSync(`tizen install -n ./build/reacttizen.wgt -s ${device}`, {
+  stdio: "inherit",
+});
